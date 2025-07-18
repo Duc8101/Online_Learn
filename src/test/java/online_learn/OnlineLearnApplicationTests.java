97d35b3abb2b7639e44896f3c020032571d850e8
Duc8101 <vegitobasicinstict1810@gmail.com>
2025-07-18 14:33:03 +0700

    .

---
+++ src/test/java/online_learn/OnlineLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package online_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
